5029fc5941867bcc2422e7906c0c1ff8de0bf7f0
Eunsang <nicolaus1207@gmail.com>
2020-03-16 06:29:13 +0900

    hello world

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+/*
+    The very frist Java program.
+*/
+class HelloWorld {
+  // A Java program begins with a call to main().
+  public static void main(String args[]) {
+    System.out.println("Hello, world!");
+  }
+}
