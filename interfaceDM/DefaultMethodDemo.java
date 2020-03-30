626c502fee67272cdb92cd5d0e800ee211de2491
EunsangJeon <eunsang.jeon.127@gmail.com>
2020-03-30 02:08:20 +0000

    finished inheritance

---
+++ interfaceDM/DefaultMethodDemo.java
@@ -0,0 +1,9 @@
+package interfaceDM;
+
+class DefaultMethodDemo {
+    public static void main(String args[]){
+        MyIFImp obj = new MyIFImp();
+        System.out.println(obj.getUserID());
+        System.out.println(obj.getAdminID());
+    }
+}
