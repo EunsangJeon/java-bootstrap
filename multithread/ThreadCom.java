4412be9e180d8a49b805211741a0e51f364e184d
Eunsang <nicolaus1207@gmail.com>
2020-04-16 22:32:37 +0900

    notify and wait done

--- multithread/ThreadCom.java
+++ multithread/ThreadCom.java
@@ -49,8 +49,33 @@ class MyThreadCom implements Runnable {
         ttOb = tt;
         thrd.start();
     }
+
+    public void run() {
+        if(thrd.getName().compareTo("Tick") == 0) {
+            for(int i = 0; i < 5; i++) {
+                ttOb.tick(true);
+            }
+            ttOb.tick(false);
+        }
+        else {
+            for(int i = 0; i < 5; i++) {
+                ttOb.tock(true);
+            }
+            ttOb.tock(false);
+        }
+    }
 }
 
 public class ThreadCom {
-
+    public static void main(String args[]) {
+        TickTock tt = new TickTock();
+        MyThreadCom mt1 = new MyThreadCom("Tick", tt);
+        MyThreadCom mt2 = new MyThreadCom("Tock", tt);
+        try {
+            mt1.thrd.join();
+            mt2.thrd.join();
+        } catch(InterruptedException exc) {
+            System.out.println("Main thread interruped.");
+        }
+    }
 }
